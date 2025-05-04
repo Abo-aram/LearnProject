30ce2328bd76968a9710ef725c000bffeed071d8
Rawezh Aram <lukannlukann936@gmail.com>
2025-05-04 23:01:10 +0300

    added about and contact

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+    <h2 class="text-3xl font-bold">About Us</h2>
+    <p class="mt-4 text-lg text-gray-600">We are a company dedicated to providing the best services.</p>
+    <p class="mt-4 text-lg text-gray-600">Our mission is to deliver quality and excellence in everything we do.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/costumPage',function(){
-    return view('costumPage');
-});
-
-Route::view('/third','costumPage');
+Route::view('/','home')->name('home');
+Route::view('/about','aboutUs')->name('about');
+Route::view('/contact','contact')->name('contact');
 
