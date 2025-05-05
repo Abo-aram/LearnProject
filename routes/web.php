b9cca40fb3b596bc9670bae0c9826ad4d3554843
Rawezh Aram <lukannlukann936@gmail.com>
2025-05-05 23:30:53 +0300

    made databse,use eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::view('/','home')->name('home');
+
+
+
+//calling the function index in the controller HomeController
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/about','aboutUs')->name('about');
 Route::view('/contact','contact')->name('contact');
+Route::view('/article','article')->name ('article');
 
