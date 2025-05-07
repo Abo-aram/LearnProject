e7da5fdedc4f4eb582f44b305bff78d58862104c
Rawezh Aram <lukannlukann936@gmail.com>
2025-05-07 15:22:35 +0300

    handeled categories and posts

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,7 +6,7 @@
     <title>Blog Homepage</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100">
+<body class="bg-gray-100 p-4">
 <header class="bg-white shadow-md p-4 flex justify-between items-center container mx-auto mt-6 rounded-lg">
     <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-800">My Blog</a>
     <nav>
